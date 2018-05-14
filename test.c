03efd6685358b5a985b1124505ee1d528b40c1e8
Assaf-Haik-Barouch <Assaf98@gmail.com>
2018-05-14 22:06:07 +0200

    better tests

--- test.c
+++ test.c
@@ -8,10 +8,14 @@
 
 int main()
 {
+	printf("start_lottery_scheduler:\n");
     start_lottery_scheduler();
+	printf("start_orig_scheduler:\n");
     start_orig_scheduler();
+	printf("set_max_tickets:\n");
     set_max_tickets(2000);
-	print_pool_level();
+	//printf("start_lottery_scheduler:\n");
+	//print_pool_level();
     return 0;
 
 }
